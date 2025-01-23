be51f3613167474819ad2ae435ddb4283f66fd5f
Jacob Preciado <jacobpreciado@gmail.com>
2025-01-22 20:30:58 -0700

    week 3 encapsulation learning activities

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction defaultFraction = new Fraction();
+        Console.WriteLine(defaultFraction.GetFractionString());
+        Console.WriteLine(defaultFraction.GetDecimalValue());
+
+        Fraction wholeNumberFraction = new Fraction(5);
+        Console.WriteLine(wholeNumberFraction.GetFractionString());
+        Console.WriteLine(wholeNumberFraction.GetDecimalValue());
+
+        Fraction fraction1 = new Fraction(3, 4);
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(1, 3);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
     }
 }
